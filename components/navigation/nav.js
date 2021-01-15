7101d4b353fc2647e6e31cf8c8258924e4569394
Michał Maciejewski <maciejewskimichal7@gmail.com>
2021-01-15 09:58:28 +0100

    add nav items

--- components/navigation/nav.js
+++ components/navigation/nav.js
@@ -1,20 +1,54 @@
+import { MdDashboard, MdAssignmentInd } from 'react-icons/md'
+import { FaBook } from 'react-icons/fa'
+import { GiLightBulb } from 'react-icons/gi'
+import { IoMdSettings} from 'react-icons/io'
+import { IoChatbubblesSharp } from 'react-icons/io5'
 import styles from "./nav.module.scss";
 
 const navTree = [
   {
     label: "Dashboard",
-    icon: "MdDashboard"
+    icon: MdDashboard
   },
   {
     label: "My Class",
-    icon: "IoMdBookmarks"
-  }
+    icon: FaBook
+  },
+  {
+    label: "Instructors",
+    icon: MdAssignmentInd,
+  },
+  
+  {
+    label: "Courses",
+    icon: GiLightBulb,
+  },
+  
+  {
+    label: "Group Chat",
+    icon: IoChatbubblesSharp,
+  },
+  
+  {
+    label: "Settings",
+    icon: IoMdSettings,
+  },
 ];
 
 const Navigation = () => {
   return (
     <nav className={styles.wrapper}>
-      <ul></ul>
+      <ul>
+        {navTree.map(item => {
+          const Icon = item.icon;
+          return (
+            <li key={item.label}>
+              <Icon />
+              {item.label}
+            </li>
+          )
+        })}
+      </ul>
     </nav>
   );
 };
